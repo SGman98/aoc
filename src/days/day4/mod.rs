98d3bf25fd26981a199fbe8a060a63d53ea53530
SGman98 <bastiangarnica@gmail.com>
2024-12-11 14:30:19 -0500

    feat: add day4 part2 solution

--- src/days/day4/mod.rs
+++ src/days/day4/mod.rs
@@ -76,8 +76,34 @@ pub fn part1(input: &str) -> Result<i32, &'static str> {
     Ok(count)
 }
 
-pub fn part2(_input: &str) -> Result<i32, &'static str> {
-    unimplemented!()
+pub fn part2(input: &str) -> Result<i32, &'static str> {
+    let mut count = 0;
+
+    let mat = input
+        .lines()
+        .map(|line| line.trim().chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    for i in 0..mat.len() {
+        for j in 0..mat[i].len() {
+            if mat[i][j] == 'A' && i >= 1 && i < mat.len() - 1 && j >= 1 && j < mat.len() - 1 {
+                match (
+                    mat[i - 1][j - 1],
+                    mat[i + 1][j + 1],
+                    mat[i - 1][j + 1],
+                    mat[i + 1][j - 1],
+                ) {
+                    ('M', 'S', 'M', 'S')
+                    | ('M', 'S', 'S', 'M')
+                    | ('S', 'M', 'M', 'S')
+                    | ('S', 'M', 'S', 'M') => count += 1,
+                    _ => (),
+                }
+            }
+        }
+    }
+
+    Ok(count)
 }
 
 #[cfg(test)]
@@ -103,6 +129,18 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        unimplemented!()
+        let input = r#"MMMSXXMASM
+                       MSAMXMSMSA
+                       AMXSXMAAMM
+                       MSAMASMSMX
+                       XMASAMXAMM
+                       XXAMMXXAMA
+                       SMSMSASXSS
+                       SAXAMASAAA
+                       MAMMMXMMMM
+                       MXMXAXMASX"#;
+
+        let result = part2(input).unwrap();
+        assert_eq!(result, 9);
     }
 }
